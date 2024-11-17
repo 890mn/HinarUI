d2ea75c8cca37320299eb87878e144ca8210a767
Hinar-atAPE660 <linkjoestar402212@gmail.com>
2024-11-17 20:01:30 +0800

    init

--- example/lib/HinarUI/UI.cpp
+++ example/lib/HinarUI/UI.cpp
@@ -605,16 +605,45 @@ void module_LIGHT() {
     menu.drawFrame();
 
     display.setCursor(1, 20);
+    display.setTextSize(2);
     display.print("LIGHT");
     display.display();
+    display.setTextSize(1);
 }
 
 void module_TIME() {
+    // Wait for DS3231
+    while (true) {
+        display.clearDisplay();
+        menu.drawTopBar();
+        menu.drawFrame();
+        uint32_t total_seconds = RTC_Time();
+    
+        uint32_t hours = total_seconds / 3600;
+        uint32_t minutes = (total_seconds % 3600) / 60;
+        uint32_t seconds = total_seconds % 60;
 
+        display.fillRect(17, 30, 100, 8, BLACK);
+        display.setCursor(17, 30);
+        display.setTextSize(2);
+        display.printf("%02d:%02d:%02d", hours, minutes, seconds);
+
+        display.display();
+        display.setTextSize(1);
+        if (digitalRead(KEY_BACK) == LOW) break;
+    }
 }
 
 void module_DHT11() {
+    display.clearDisplay();
+    menu.drawTopBar();
+    menu.drawFrame();
 
+    display.setCursor(1, 20);
+    display.setTextSize(2);
+    display.print("DHT11");
+    display.display();
+    display.setTextSize(1);
 }
 
 void module_UICORE() {
